b6c4c9a574e322ccc3e30747f0a1031e29ea8535
adssadasddasda <rachedaemon666@gmail.com>
2015-04-22 00:53:15 +0200

    Update H1.c
    grid_print_dot(cells) now working, fixed the tons of Warnings (only 1 left)

--- H1.c
+++ H1.c
@@ -5,8 +5,8 @@
 typedef struct _cells *cells;
 typedef struct _cells {
   int contents;
-  struct cell* rchild;
-  struct cell* dchild;
+  cells rchild;
+  cells dchild;
 } cell;
 
 cells grid_init(int num_columns, int num_rows) {
@@ -38,30 +38,43 @@ void grid_print(cells grid) {
     here = here->rchild;
   }
   while (row[0]!=NULL) {
-    printf("%s","| ");
+    printf("| ");
     for (i=0; i<columns; i++) {
       printf("%d ", row[i]->contents);
       row[i] = row[i]->dchild;
     }
-    printf("%s\n", "|");
+    printf("|\n");
   }
 }
 
+
+//The surrounding construct for the DOT Language.
 void grid_print_dot(cells grid) {
-  
+    FILE* file = fopen("H1.txt", "w");
+    fprintf(file, "digraph {\n");
+    grid_print_dot_cells(file, grid);
+    fprintf(file, "}");
+    fclose(file);
+}
+
+//Recursive writing the Adresses of the childs to the file.
+void grid_print_dot_cells(FILE* file, cells grid) {
+    if (grid!=NULL) {
+        if (grid->dchild!=NULL) fprintf(file, "  M0x%p -> M0x%p\n", grid, grid->dchild);
+        if (grid->rchild!=NULL) fprintf(file, "  M0x%p -> M0x%p\n", grid, grid->rchild);
+        grid_print_dot_cells(file, grid->dchild);
+        grid_print_dot_cells(file, grid->rchild);
+    }
 }
 
 // Fills the specified cell with the content.
 void grid_fillCell(int column, int row, int content, cells grid) {
-  if (column=0&row=0) {
-    grid->contents = content;
-  }
-  if (column>0) {
-    grid_fillCell(column-1, row, content, grid->rchild);
-  }
-  if (row>0) {
-    grid_fillCell(column, row-1, content, grid->dchild);
-  }
+    if (grid!=NULL){
+        if (column>0) grid_fillCell(column-1, row, content, grid->rchild);
+        else if (row>0) grid_fillCell(column, row-1, content, grid->dchild);
+        else grid->contents = content;
+    }
+    else printf("Target Cell is outside the Grid!\n");
 }
 
 // Fills the Grid with random Numbers in the range between min and max.
@@ -81,7 +94,10 @@ void grid_fillrandom(cells grid, int min, int max) {
 
 int main() {
   cells h = grid_init(4, 5); //(columns, rows)
+  printf("M0x%p\n", h);
   grid_fillrandom(h, 1, 2); //(grid, minimum, maximum)
+  grid_fillCell(3, 2, 0, h);
   grid_print(h); //(grid)
+  grid_print_dot(h);
   return 0;
 }
