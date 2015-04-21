ba344cc6666740601aac74f93e1f088520674fbf
adssadasddasda <rachedaemon666@gmail.com>
2015-04-22 01:34:21 +0200

    Renamed cells, added some comments
    renamed cells to cell and cell to single_cell for better readability

--- H1.c
+++ H1.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 
-typedef struct _cells *cells;
-typedef struct _cells {
+//The typedef struckt of cells
+typedef struct _cell *cell;
+typedef struct _cell {
   int contents;
-  cells rchild;
-  cells dchild;
-} cell;
+  cell rchild;
+  cell dchild;
+} single_cell;
 
-cells grid_init(int num_columns, int num_rows) {
-  cells new = malloc(sizeof(cell));
+//Initialises a grid with set columns and rows.
+cell grid_init(int num_columns, int num_rows) {
+  cell new = malloc(sizeof(single_cell));
   new->contents = 0;
   new->rchild = NULL;
   new->dchild = NULL;
@@ -23,15 +25,16 @@ cells grid_init(int num_columns, int num_rows) {
   return new;
 }
 
-void grid_print(cells grid) {
+//Prints the grid.
+void grid_print(cell grid) {
   int columns = 1;
   int i;
-  cells here = grid;
+  cell here = grid;
   while (here->rchild!=NULL) {
     columns++;
     here = here->rchild;
   }
-  cells row[columns];
+  cell row[columns];
   here = grid;
   for (i=0; i<columns; i++) {
     row[i] = here;
@@ -49,7 +52,7 @@ void grid_print(cells grid) {
 
 
 //The surrounding construct for the DOT Language.
-void grid_print_dot(cells grid) {
+void grid_print_dot(cell grid) {
     FILE* file = fopen("H1.txt", "w");
     fprintf(file, "digraph {\n");
     grid_print_dot_cells(file, grid);
@@ -58,29 +61,29 @@ void grid_print_dot(cells grid) {
 }
 
 //Recursive writing the Adresses of the childs to the file.
-void grid_print_dot_cells(FILE* file, cells grid) {
+void grid_print_dot_cells(FILE* file, cell grid) {
     if (grid!=NULL) {
-        if (grid->dchild!=NULL) fprintf(file, "  M0x%p -> M0x%p\n", grid, grid->dchild);
-        if (grid->rchild!=NULL) fprintf(file, "  M0x%p -> M0x%p\n", grid, grid->rchild);
-        grid_print_dot_cells(file, grid->dchild);
-        grid_print_dot_cells(file, grid->rchild);
+      if (grid->dchild!=NULL) fprintf(file, "  M0x%p -> M0x%p\n", grid, grid->dchild);
+      if (grid->rchild!=NULL) fprintf(file, "  M0x%p -> M0x%p\n", grid, grid->rchild);
+      grid_print_dot_cells(file, grid->dchild);
+      grid_print_dot_cells(file, grid->rchild);
     }
 }
 
 // Fills the specified cell with the content.
-void grid_fillCell(int column, int row, int content, cells grid) {
+void grid_fillCell(int column, int row, int content, cell grid) {
     if (grid!=NULL){
-        if (column>0) grid_fillCell(column-1, row, content, grid->rchild);
-        else if (row>0) grid_fillCell(column, row-1, content, grid->dchild);
-        else grid->contents = content;
+      if (column>0) grid_fillCell(column-1, row, content, grid->rchild);
+      else if (row>0) grid_fillCell(column, row-1, content, grid->dchild);
+      else grid->contents = content;
     }
     else printf("Target Cell is outside the Grid!\n");
 }
 
 // Fills the Grid with random Numbers in the range between min and max.
-void grid_fillrandom(cells grid, int min, int max) {
-  cells column = grid;
-  cells row;
+void grid_fillrandom(cell grid, int min, int max) {
+  cell column = grid;
+  cell row;
   srand(time(NULL));
   while (column!=NULL) {
     row = column;
@@ -93,7 +96,7 @@ void grid_fillrandom(cells grid, int min, int max) {
 }
 
 int main() {
-  cells h = grid_init(4, 5); //(columns, rows)
+  cell h = grid_init(4, 5); //(columns, rows)
   printf("M0x%p\n", h);
   grid_fillrandom(h, 1, 2); //(grid, minimum, maximum)
   grid_fillCell(3, 2, 0, h);
